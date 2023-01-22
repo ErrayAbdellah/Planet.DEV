829b6f3baad72142da9cfd131ba4225d05433cc9
ErrayAbdellah <mahdiabdllah121@gmail.com>
2023-01-22 13:57:36 +0100

    add multiple insert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 
-
+let lenght = 1;
+$('#linghtCoount').val(lenght);
 $("#home").css({'display':'block'});
 $("#dashboard").css({'display':'none'});
 $("#editSet").css({'display':'none'});
@@ -54,3 +55,35 @@ function addEdit(){
 }
 
 
+
+
+function addArticle(){
+    lenght++;
+    $('#linghtCoount').val(lenght);
+// console.log(lenght);
+    $("#addNewArticle").append(`<div class="d-flex justify-content-end w-100 align-items-center" >
+                                    <div class="mx-5">
+                                        <select class="form-select form-select-lg mb-3 shadow  bg-body-tertiary "name="categorySelect" id="">
+                                            <option selected>..Chose Categorie..</option>
+                                            <?php foreach ($cat as $row){ ?>
+                                            <option value="<?= $row['id'] ?>"><?= $row['name'] ?></option>
+                                            <?php }?>
+                                        </select>
+                                    </div>
+                                    <div class="w-75">
+                                        <div class="my-1 ">
+                                                <input class="form-control w-25" type="text" name="txtTitle" id="txtTitle" placeholder="Title">
+                                            </div> 
+                                            <textarea class="w-100" name="txtArticle" id="txtArticle" cols="70" rows="6"></textarea>
+                                            </div>
+                                            <button class="btn btn-danger" type="button" id="btnRemove">Remove</button>
+                                            </div>`);
+
+}
+
+
+$('body').on('click','#btnRemove',function(){
+    $(this).closest('div').remove();
+    lenght--;
+    $('#linghtCoount').val(lenght);
+});
--- views/add.php
+++ views/add.php
@@ -3,39 +3,43 @@
 $cat = Categorie::show();
 
 if(isset($_POST['bntADD'])){
-    $title = $_POST['txtTitle'];
-    $content = $_POST['txtArticle'];
-    $admin = 1; 
-    $date = date('y-m-d');
 
-    Article::create($title, $content, $admin, $date, 1);
-    // $article = new Article($title,$content,$admin,$date)-> ;
+    for ($i = 0; $i < count($_POST['linghtCoount']); $i++){
+        $title = $_POST['txtTitle'][$i];
+        $content = $_POST['txtArticle'][$i];
+        $category = $_POST['categorySelect'][$i];
+        $admin = 1; 
+        $date = date('y-m-d');
+        // echo "<script>alert('$category')</script>";
+        Article::create($title, $content, $admin, $date, $category);
+    }
 }
 ?>
 <section id="add"  class="col mt-5">
     <h4 class="text-center my-5">Setting Article</h4>
     <form action="" method="post">
-        <div class="d-flex justify-content-end w-100 align-items-center">
-            <div class="mx-5">
-                <select class="form-select form-select-lg mb-3 shadow  bg-body-tertiary "name="" id="">
-                    <option selected>..Chose Categorie..</option>
-                    <?php foreach ($cat as $row){ ?>
-                    <option value="<?= $row['id'] ?>"><?= $row['name'] ?></option>
-                    <?php }?>
-                </select>
+        <div id="addNewArticle">
+            <div class="d-flex justify-content-end w-100 align-items-center" >
+                <div class="mx-5">
+                    <select class="form-select form-select-lg mb-3 shadow  bg-body-tertiary "name="categorySelect[]" id="">
+                        <option selected>..Chose Categorie..</option>
+                        <?php foreach ($cat as $row){ ?>
+                        <option value="<?= $row['id'] ?>"><?= $row['name'] ?></option>
+                        <?php }?>
+                    </select>
+                </div>
+                <div class="w-75">
+                    <div class="my-1">
+                            <input class="form-control w-25" type="text" name="txtTitle[]" id="txtTitle" placeholder="Title">
+                    </div> 
+                    <textarea class="w-100" name="txtArticle[]" id="txtArticle" cols="70" rows="6"></textarea>
+                </div>
             </div>
-            
-            <div class="w-75">
-            <div class="w-25 my-1">
-                <label for="title"></label>
-                <input class="form-control" type="text" name="txtTitle" id="txtTitle" placeholder="Title">
-            </div> 
-                <textarea class="w-100" name="txtArticle" id="txtArticle" cols="70" rows="6"></textarea>
-            </div>  
         </div>
         <div class="d-flex justify-content-end mt-3">
+            <input type="hidden" id="linghtCoount" name="linghtCoount">
             <button type="button" class="btn btn-danger mx-3" onclick="cancelSettings()">Cancel</button>
-            <button type="button" class="btn btn-info mx-3" onclick="">Add new Article</button>
+            <button type="button" class="btn btn-info mx-3" onclick="addArticle()">Add new Article</button>
             <input type="submit" class="btn btn-secondary mx-3" name="bntADD" value="Add">
         </div>
     </form>
